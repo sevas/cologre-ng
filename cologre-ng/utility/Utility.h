75c880f7cd8ea3609b8572aa45e8c1e5bd65223f
sevas <f.degroef@gmail.com>
2010-08-19 13:09:48 +0200

    added a 9 params toString()

--- cologre-ng/utility/Utility.h
+++ cologre-ng/utility/Utility.h
@@ -2,8 +2,11 @@
 #ifndef __110810_UTILITY_H__
 #define __110810_UTILITY_H__
 
-
 #include <string>
+#include <sstream>
+
+namespace cologreng{
+namespace utility{
 
 std::string makeFullUri(const std::string &_scheme, const std::string &_path);
 std::string convertUriToPath(const std::string &_uri);
@@ -78,11 +81,22 @@ inline std::string toString (T1 p1, T2 p2, T3 p3, T4 p4, T5 p5, T6 p6, T7 p7, T8
     return stream.str();
 }
 
+
+template<typename T1,  typename T2,  typename T3, typename T4, typename T5, typename T6, typename T7, typename T8, typename T9 >
+inline std::string toString (T1 p1, T2 p2, T3 p3, T4 p4, T5 p5, T6 p6, T7 p7, T8 p8, T9 p9)
+{
+    std::ostringstream stream;
+    stream << p1 << p2 << p3 << p4 << p5 << p6 << p7 << p8 << p9;
+    return stream.str();
+}
+
 template< >
 inline std::string toString<bool> (bool _value)
 {
     return _value ? "true" : "false";
 }
 
+
+}}
 
 #endif 
